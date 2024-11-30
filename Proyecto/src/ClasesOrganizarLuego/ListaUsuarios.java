22813d90a3a9f849dfea14d8a6842ef21e759099
Jesús Acosta <160048127+Acosti-ctrl-z@users.noreply.github.com>
2024-11-30 13:48:59 -0400

    Algunos métodos (versión 1) fueron añadidos
    Se añadieron: leerLista, agregarUsuario a listaUsuarios.
    Se añadieron métodos básicos a Usuario (getters, setters y constructor).

--- Proyecto/src/ClasesOrganizarLuego/ListaUsuarios.java
+++ Proyecto/src/ClasesOrganizarLuego/ListaUsuarios.java
@@ -3,5 +3,29 @@ package ClasesOrganizarLuego;
 import java.util.List;
 
 public class ListaUsuarios {
-    private List<Usuario> usuarioList;
+    private List<Usuario> listaUsuarios;
+
+    public ListaUsuarios() {
+        this.listaUsuarios=null;
+    }
+
+    public void leerLista() {
+        if (listaUsuarios == null || listaUsuarios.isEmpty()) {
+            System.out.println("No hay ningún usuario registrado");
+        } else {
+            int posicion = 1;
+            for (Usuario usuario : this.listaUsuarios) {
+                System.out.print(posicion + ". ");
+                System.out.println("Alias: " + usuario.getAlias() + ".");
+                System.out.println("Email: " + usuario.getCorreo() + ".");
+                System.out.println("Palabras jugadas: " + usuario.getPalabrasJugadas() + ".");
+                System.out.println("Palabras jugadas: " + usuario.getPuntajeAcumulado() + ".");
+                posicion = posicion + 1;
+            }
+        }
+    }
+
+    public void agregarUsuario(Usuario usuarioNuevo){
+        this.listaUsuarios.add(usuarioNuevo);
+    }
 }
--- Proyecto/src/ClasesOrganizarLuego/Usuario.java
+++ Proyecto/src/ClasesOrganizarLuego/Usuario.java
@@ -7,4 +7,42 @@ public class Usuario {
     private int puntajeAcumulado;
     private int palabrasJugadas;
 
+    public Usuario(String alias, String correo){
+        this.alias=alias;
+        this.correo=correo;
+        this.palabrasJugadas=0;
+        this.puntajeAcumulado=0;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void aumentarPuntajeAcumulado(int puntajeAcumulado) {
+        this.puntajeAcumulado += puntajeAcumulado;
+    }
+
+    public void aumentarPalabrasJugadas(int palabrasJugadas) {
+        this.palabrasJugadas += palabrasJugadas;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getPuntajeAcumulado() {
+        return puntajeAcumulado;
+    }
+
+    public int getPalabrasJugadas() {
+        return palabrasJugadas;
+    }
 }
