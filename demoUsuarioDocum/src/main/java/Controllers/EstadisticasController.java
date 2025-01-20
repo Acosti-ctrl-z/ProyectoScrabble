3bd86780f7d572af63206114069f32b98109e3b2
Jesús Acosta <160048127+Acosti-ctrl-z@users.noreply.github.com>
2025-01-19 20:39:41 -0400

    Actualizacion de EstadisticasController
    Ahora si se ve como una lista

--- demoUsuarioDocum/src/main/java/Controllers/EstadisticasController.java
+++ demoUsuarioDocum/src/main/java/Controllers/EstadisticasController.java
@@ -12,9 +12,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -62,33 +65,50 @@ public class EstadisticasController {
 
     @FXML
     private Label partidasText;
+
+    @FXML
+    private ListView<String> viewNames=new ListView<String>();
+
+    @FXML
+    private ListView<String> viewCorreo=new ListView<String>();
+
+    @FXML
+    private ListView<String> viewPuntaje=new ListView<String>();
+
+    @FXML
+    private ListView<String> viewPartidas=new ListView<String>();
+
     @FXML
     /**
      * método para mostrar los datos de un jugador
      */
     protected void estadisticasJugador(){
+
         Device device = new Device();
         Commands estadisticasMostrar = new EstadisticasMostrarCommand(device);
         Boton boton = new Boton();
-
         this.users = LectorJson.leerDatos(this.users, "ListaUsuarios");
-        int indice = Integer.parseInt(indiceJtext.getText());
-        if (users.getListaUsuarios()!= null && !users.getListaUsuarios().isEmpty()) {
-            if (indice >= 1 && indice <= users.getListaUsuarios().size()) {
-                System.out.println("Objetivo encontrado.");
-                Usuario user = users.getListaUsuarios().get(indice - 1);
-                nombreText.setText("Nombre: " + user.getAlias());
-                correoText.setText("Correo: " +user.getCorreo());
-                palabrasText.setText("Palabras Jugadas: " + user.getPalabrasJugadas());
-                puntajeText.setText("Puntaje obetenido: " + user.getPuntajeAcumulado());
-                partidasText.setText("Partidas Ganadas: " + user.getPartidasGanadas());
-                boton.setCommands(estadisticasMostrar);
-                boton.pressButton(estadisticasText);
-            } else {
-                estadisticasText.setText("Objetivo no encontrado, vuelva a intentar.");
-            }
-        } else {
-            System.out.println("No hay ningún usuario registrado");
+
+        ObservableList<String> observableAlias =FXCollections.observableArrayList();
+        for(Usuario user: this.users.getListaUsuarios()){
+            observableAlias.add(user.getAlias());
         }
+        viewNames.setItems(observableAlias);
+
+        ObservableList<String> observableCorreo =FXCollections.observableArrayList();
+        for(Usuario user: this.users.getListaUsuarios()){
+            observableCorreo.add(user.getCorreo());
+        }
+        viewCorreo.setItems(observableCorreo);
+        ObservableList<String> observablePuntos =FXCollections.observableArrayList();
+        for(Usuario user: this.users.getListaUsuarios()){
+            observablePuntos.add(Integer.toString(user.getPuntajeAcumulado()));
+        }
+        viewPuntaje.setItems(observablePuntos);
+        ObservableList<String> observablePartidas =FXCollections.observableArrayList();
+        for(Usuario user: this.users.getListaUsuarios()){
+            observablePartidas.add(Integer.toString(user.getPartidasGanadas()));
+        }
+        viewPartidas.setItems(observablePartidas);
     }
 }
